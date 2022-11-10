8b82a7fe5376fc08c21a18bb9831cf9cf143bdfe
Vladislav <91069241+Asphodell@users.noreply.github.com>
2022-11-10 19:16:11 +0300

    task 13

---
+++ 13.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+void check_answer(int a, int b, int answer);
+
+int main()
+{
+    int first_rand = std::rand() % 9;
+    std::cout << "a = " << first_rand << std::endl;
+    int second_rand = std::rand() % 9;
+    std::cout << "b = " << second_rand << std::endl;
+    int answer;
+    std::cout << "Enter answer a * b = ";
+    std::cin >> answer;
+    check_answer(first_rand, second_rand, answer);
+}
+
+void check_answer(int a, int b, int answer) {
+    int result = a * b;
+    if (result == answer) {
+        std::cout << "All right!";
+    }
+    else {
+        std::cout << "No :P";
+    }
+}
