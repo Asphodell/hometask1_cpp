c9e1b728705d9af1fbaa1b08321541c59412e87e
Vladislav <91069241+Asphodell@users.noreply.github.com>
2022-11-10 18:19:58 +0300

    task 9

---
+++ 9.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+int finding_hours(int sec);
+int finding_minutes(int sec);
+
+int main() {
+	double k;
+	std::cout << "Enter k = " << std::endl;
+	std::cin >> k;
+	std::cout << "hours: " << finding_hours(k) << std::endl;
+	std::cout << "minutes: " << finding_minutes(k);
+}
+
+int finding_hours(int sec) {
+	int answer = sec / 60 / 60;
+	return answer;
+}
+
+int finding_minutes(int sec) {
+	int answer = sec / 60;
+	return answer;
+}
