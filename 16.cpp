43c29d7ee5f73c4121d8c097ada54d25651ef42e
Vladislav <91069241+Asphodell@users.noreply.github.com>
2022-11-10 19:55:54 +0300

    task 16

---
+++ 16.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+void checking(int a);
+
+int main()
+{
+    int a;
+    std::cout << "Enter number = ";
+    std::cin >> a;
+    checking(a);
+}
+
+void checking(int a) {
+    int count = 0;
+    int sum_of_first_3 = 0;
+    int sum2 = 0;
+    while (a > 0) {
+        if (count < 3) { 
+            sum_of_first_3 = sum_of_first_3 + a % 10;
+            count += 1;
+        }
+        else {
+            sum2 = sum2 + a % 10;
+        }
+        a = a / 10;
+    }
+    if (sum_of_first_3 == sum2) {
+        std::cout << "Happy!!";
+    }
+    else {
+        std::cout << "Not happy.";
+    }
+}
