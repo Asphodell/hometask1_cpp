a548bd2b2348a0b028f3806da4c30ccff1c1abb8
Vladislav <91069241+Asphodell@users.noreply.github.com>
2022-11-10 18:57:23 +0300

    task 12

---
+++ 12.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+void recommendation(float weight, float height);
+
+int main()
+{
+    float weight;
+    float height;
+    std::cout << "Enter your weight = ";
+    std::cin >> weight;
+    std::cout << "Enter your height = ";
+    std::cin >> height;
+    recommendation(weight, height);
+}
+
+void recommendation(float weight, float height) {
+    if ((height - weight) > 100) {
+        std::cout << "Need to get fatter";
+    }
+    if ((height - weight) < 100) {
+        std::cout << "Need to lose weight";
+    }
+    if ((height - weight) == 100) {
+        std::cout << "Perfect";
+    }
+}
