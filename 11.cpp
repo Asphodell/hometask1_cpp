32963759f86baecfaef0ff1d4defeb9aac3eb56b
Vladislav <91069241+Asphodell@users.noreply.github.com>
2022-11-10 18:45:33 +0300

    task 11

---
+++ 11.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+double sum_with_sale(double sum);
+
+int main(){  
+	double sum;
+	std::cout << "Enter sum = ";
+	std::cin >> sum;
+	std::cout << "Sum with sale is: " << sum_with_sale(sum);
+}
+
+double sum_with_sale(double sum) {
+	if (sum > 1000) {
+		double result_sum = sum - sum * 0.1;
+		return result_sum;
+	}
+	else {
+		double result_sum = sum;
+		return result_sum;
+	}
+}
